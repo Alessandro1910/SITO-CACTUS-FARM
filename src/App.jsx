cd015a83cd8a3563df633e1b040d93df33e562a6
Alessandro1910 <alessandroturacci1910@gmail.com>
2025-02-04 14:33:14 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <nav>
+    <a className='text-6xl' href="Digu">DIGU</a>
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Features</a>
+    <button>ACCEDI</button>
+    <button>ISCRIVITI</button>
+   </nav>
+    </>
+  )
+}
+
+export default App
